31de770ee6fc97c192ca0ed7d3717a2e77253018
Onuralp Özkan <76494187+onuralpozkan@users.noreply.github.com>
2021-04-21 12:37:19 +0300

    initialize project

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import {useEffect,useState} from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [second, setSecond] = useState(0)
+  // useEffect(() => {
+  //   setTimeout(()=>{
+  //     setCount(prevCount => prevCount + 1)
+  //   },1000)
+    
+  //   // return () => {
+  //   //   cleanup
+  //   // }
+  // }, [count])
+  const [isStarted, setIsStarted] = useState(false)
+  // const sw = setInterval(() => {
+  //   setCount(prevCount => prevCount + 1)
+  // }, 1000);
+  const startWatch = () => {
+    setIsStarted(!isStarted)
+
+  }
+  useEffect(() => {
+    let interval = null
+    if(isStarted){
+      interval = setInterval(()=>{
+        if(count < 60){
+          setCount(prevCount => prevCount + 1)
+        }else if(count === 60) {
+          setCount(0)
+          setSecond(prevS => prevS + 1)
+        }
+        
+      },10)
+    } 
+    return () => {
+      clearInterval(interval)
+    }
+  }, [isStarted,count])
+  return (
+    <div className="App">
+      <h1>
+        StopWatch
+      </h1>      
+
+      <h1>{second} : {count}</h1>
+      
+      <button onClick={startWatch}>{isStarted ? 'Stop' : 'Start'}</button>
+      <button onClick={()=>setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
