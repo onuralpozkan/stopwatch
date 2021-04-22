335b069b798d0ce9f05fef9fa2c0880760a26089
onuralp.ozkan <onuralp.ozkan@linktera.com.tr>
2021-04-22 14:25:26 +0300

    time intervals added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
 import './App.css';
-import {useEffect,useState} from 'react'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [second, setSecond] = useState(0)
-  // useEffect(() => {
-  //   setTimeout(()=>{
-  //     setCount(prevCount => prevCount + 1)
-  //   },1000)
-    
-  //   // return () => {
-  //   //   cleanup
-  //   // }
-  // }, [count])
-  const [isStarted, setIsStarted] = useState(false)
-  // const sw = setInterval(() => {
-  //   setCount(prevCount => prevCount + 1)
-  // }, 1000);
+  const [count, setCount] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+  const [timeArray, setTimeArray] = useState([]);
   const startWatch = () => {
-    setIsStarted(!isStarted)
-
-  }
-  useEffect(() => {
-    let interval = null
+    setIsStarted(!isStarted);
     if(isStarted){
-      interval = setInterval(()=>{
-        if(count < 60){
-          setCount(prevCount => prevCount + 1)
-        }else if(count === 60) {
-          setCount(0)
-          setSecond(prevS => prevS + 1)
-        }
-        
-      },10)
-    } 
-    return () => {
-      clearInterval(interval)
+      let time = second + " : " + (count < 10 ? '0' + count : count);
+      timeArray.push(time)
     }
-  }, [isStarted,count])
+  };
+  useEffect(() => {
+    let interval = null;
+    if (isStarted) {
+      interval = setInterval(() => {
+        if (count < 60) {
+          setCount((prevCount) => prevCount + 1);
+        } else if (count === 60) {
+          setCount(0);
+          setSecond((prevS) => prevS + 1);
+        }
+      }, 10);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isStarted, count]);
+  const intervals = timeArray.map((i,index) => (<li className="time-item" key={`i - ${index}`}>{index + 1}. <em>{i}</em></li>))
   return (
-    <div className="App">
-      <h1>
-        StopWatch
-      </h1>      
+    <div className="main">
+      <div className="watch">
+        <h1>StopWatch</h1>
 
-      <h1>{second} : {count}</h1>
-      
-     <div className="btns">
-     <button onClick={startWatch}>{isStarted ? 'Stop' : 'Start'}</button>
-      <button onClick={()=>{
-        setCount(0)
-        setSecond(0)
-      }}>Reset</button>
-     </div>
+        <h1>
+          {second === 0 ? null : second + ' : '}
+          {count < 10 ? '0' + count : count}
+        </h1>
 
+        <div className="btns">
+          <button onClick={startWatch}>{isStarted ? 'Stop' : 'Start'}</button>
+          <button
+            onClick={() => {
+              setCount(0);
+              setSecond(0);
+              setTimeArray([])
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="right-panel">
+        <ul className="time-list">
+          {intervals}
+          {/* <li className="time-item">
+            "02 : 22"
+          </li>
+          <li className="time-item">
+            "02 : 22"
+          </li>
+          <li className="time-item">
+            "02 : 22"
+          </li> */}
+        </ul>
+      </div>
     </div>
   );
 }
