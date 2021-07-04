97fbdb9186a51541286bf9f9369284410ffdc59b
onuralp.ozkan <onuralp.ozkan@linktera.com.tr>
2021-07-04 15:35:56 +0300

    alarm page ui

--- src/components/alarm/Alarm.jsx
+++ src/components/alarm/Alarm.jsx
@@ -4,6 +4,7 @@ import useClock from '../utils/useClock';
 import './Alarm.css';
 import uniqid from 'uniqid';
 import SingleAlarm from './SingleAlarm';
+import { Link } from 'react-router-dom';
 const Alarm = () => {
   const [alarms, setAlarms] = useState([]);
 
@@ -21,9 +22,34 @@ const Alarm = () => {
   const removeAlarm = (e, id) => {
     setAlarms((p) => p.filter((i) => i.id != id));
   };
-  const alarmBox = alarms.length > 0 ? alarms.map((i) => i.component) : 'Add alarm to set alarm :)' 
+  const alarmBox = alarms.length > 0 ? alarms.map((i) => <div key={i.id}>{i.component}</div>) : 'Add alarm to set alarm :)' 
+  const [isConfigOpen, setIsConfigOpen] = useState(false);
+  const openConfig = () => {
+    setIsConfigOpen(!isConfigOpen);
+  }
   return (
     <div className="wa-alarm-wrapper">
+      <div className="wa-alarm-top">
+        <div className="wa-home">
+          <Link to="/" className="wa-top-links">
+            <i className="las la-home"></i>
+          </Link>
+        </div>
+        <div className="wa-alarm-config" onClick={openConfig}>
+          <Link className="wa-top-links">
+            <i className="las la-cog"></i>
+          </Link>
+        </div>
+        <div className={`wa-config-box wa-config-${isConfigOpen ? 'open' : 'close'}`}>
+          <ul>
+            <li>
+              Settings
+            </li>
+            <li>Set Alarm Music</li>
+            <li><input type="file" name="" id="" /></li>
+          </ul>
+        </div>
+      </div>
       <h1 className="wa-alarm-title">Alarm</h1>
       <div className="wa-alarms-container">
         {alarmBox}
